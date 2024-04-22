1a07bdf76c282f322ccebe94be8392eb3bc9b3b7
Gianluca545 <166713625+Gianluca545@users.noreply.github.com>
2024-04-22 08:43:22 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <button classs="button">Controle de Estoque</button>
+
+    <button class="ppp">Tênis Esportivo</button>
+
+    <button class="ttt">Tênis Casual</button>
+
+    <button class="bbb">voltar</button>
+
+    
+
+    </div>
+  );
+}
+
+export default App;
