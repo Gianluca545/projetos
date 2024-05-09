292a82ee88da5f42ac359b8b51d72e2985fc0a90
Gianluca545 <166713625+Gianluca545@users.noreply.github.com>
2024-05-09 02:45:39 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
 import './App.css';
-import Logo from "./img/par branco.png"
-import Foto from "./img/eagles.png"
-import pem from "./img/download.jpeg"
+
+import Logo from './img/eagles.png'
 
 function App() {
   return (
     <div className="App">
 
-    <div>
-      <img class="nnn" src={pem} width={100} height={50}></img>
-    </div>
-
-    <div>
-      <img class="lll" src={Foto} width={150} height={100}></img>
-    </div>
-
-    <div>
-    <img  class="img" src={Logo} width={450} height={350}></img>
-
-    </div>
-
-      <h1 class="aaa">Tênis Casual: Masculino Finity</h1>
-
-      <p1 class="bbb">Masculino / Calçados / Esportivo</p1>
-
-      <h2 class="ccc">R$ 379,90</h2>
-
-      <h3 class="ddd">Cor white</h3>
-
-      <p2 class="eee">6x de R$ 53,49</p2>
-
-      <button class="fff">Masculino</button>
-
-      <button class="ggg">Feminino</button>
-
-      <button class="hhh">infantil</button>
-
-      <button class="iii">Coleções</button>
-
-      <button class="jjj">Voltar</button>
-
-      <button class="kkk">Avançar</button>
+      <div className="comp-cabecalho">
+        <img class="img" src={Logo} width={150} height={100}></img>
+      </div>
       
+      <div className="buttons">
+      <button class="button">Controle de Estoque</button>
+
+      <button class="ppp">Tênis Masculino</button>
+
+      <button class="ttt">Tênis Feminino</button>
+
+      <button class="bbb">Voltar</button>
+      </div>
+      
+      <div className="eagles">
+      <h1 class="a">E</h1>
+
+      <h1 class="b">A</h1>
+
+      <h1 class="c">G</h1>
+
+      <h1 class="d">L</h1>
+
+      <h1 class="e">E</h1>
+      
+      <h1 class="f">S</h1>
+      </div>
     </div>
   );
 }
